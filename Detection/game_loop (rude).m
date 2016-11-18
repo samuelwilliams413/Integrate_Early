f48678b8054c1fa4b146e7d177f3d7b8f2aab3d0
samuelwilliams413 <samuelwilliams413@gmail.com>
2016-11-19 09:59:07 +1000

    Adding Rude

---
+++ Detection/game_loop (rude).m
@@ -0,0 +1,30 @@
+function [matches, dominos, finalfinallines, trackingin] = game_loop(cameraParams)
+close all
+clear cam
+filename = 'IMAGE.jpg';
+
+cam = webcam('Microsoft� LifeCam Studio(TM)');
+cam.resolution = '1920x1080';
+cam.Brightness=80;
+preview(cam);
+pause(5);
+
+while 1  
+    img = get_img(cam, cameraParams);
+    imshow(img);
+    imwrite(img, filename);
+    
+    % REMOVE THIS LINE
+    img = imread(filename);
+
+    [matches, dominos, finalfinallines, trackingin] = detection(img);
+    [face_count] = make_bag_o_D(trackingin, matches, filename);
+    [the_D, face_count] = get_next_domino(face_count);
+    [the_D, face_count] = get_next_domino(face_count);
+    [the_D, face_count] = get_next_domino(face_count);
+    [path] = get_path(start, finish, face_count)
+    
+    break
+end
+
+end
