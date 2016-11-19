0e7af13d6f02788ba63a5d8bf8c6e7c85cd525b6
Ferginator <ferg_11235@hotmail.com>
2016-11-19 11:25:02 +1000

    resize resolution of image
    change world coords from mm to cm

--- Object Mapping/start_pix_to_world.m
+++ Object Mapping/start_pix_to_world.m
@@ -1,15 +1,15 @@
 function [ startpoints_world] = start_pix_to_world( mid_x, mid_y)
 %Given start centre point in pixels converts to world coordinates
 %   assuming the origin is the arm pivot point
-x_pixels = 1286.4;
-y_pixels = 723.6;
-x_world = 695;
-y_world = 395;
+x_pixels = 1261;
+y_pixels = 698;
+x_world = 69.5;
+y_world = 39.5;
 
 x_mm_per_pix = x_world/x_pixels;
 y_mm_per_pix = y_world/y_pixels;
 
-origin_offset = 60;
+origin_offset = 6.0;
 
 start_mid_x_mm = (mid_x*x_mm_per_pix)-(x_world*0.5);
 start_mid_y_mm = (y_pixels-mid_y)*y_mm_per_pix+origin_offset;
