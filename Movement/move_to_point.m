d4bab152d23401a89552fa2889a3663d99600e18
samuelwilliams413 <samuelwilliams413@gmail.com>
2016-11-19 11:57:11 +1000

    Taking credit for ryans work

--- Movement/move_to_point.m
+++ Movement/move_to_point.m
@@ -1,4 +1,10 @@
 function move_to_point(x1,y1,z1,x2,y2)
-move_dom_x(x1,y1,z1,x2)
-move_dom_y(x2,y1,z1,y2)
+if y1 >= 10
+    move_dom_x(x1,y1,z1,x2)
+    move_dom_y(x2,y1,z1,y2)
+else
+    move_dom_y(x2,y1,z1,y2)
+     move_dom_x(x1,y1,z1,x2)
+end
+
 end
