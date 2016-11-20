358dc56f3d2534e60f091ce31f985ba67aedd2ea
brookeschafer <brookeschafer@live.com.au>
2016-11-20 15:23:40 +1000

    object mapping
    fixed relationship between cm and pix for new camera positioning

--- Object Mapping/start_and_endpoints_world.m
+++ Object Mapping/start_and_endpoints_world.m
@@ -26,8 +26,8 @@ y_spacing = 3.0;
 
 world = [];
 for i = 1:length(data(:,1))
-    j=(data(i,2)*x_cm_per_pix)-(x_world*0.5)-3.5;
-    k=(y_pixels-data(i,3))*y_cm_per_pix+origin_offset-2.3;
+    j=(data(i,2)*x_cm_per_pix)-(x_world*0.5)-2.44;
+    k=(y_pixels-data(i,3))*y_cm_per_pix+origin_offset-3.74;
     world(i,1)=j;
     world(i,2)=k;
 end
