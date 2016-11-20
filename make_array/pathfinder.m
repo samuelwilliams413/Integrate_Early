6cf07258dfb58cf76dfa5762e90c0f97550f64c1
samuelwilliams413 <samuelwilliams413@gmail.com>
2016-11-20 18:28:41 +1000

    Pathfinder.001

---
+++ make_array/pathfinder.m
@@ -0,0 +1,34 @@
+function [path] =  pathfinder (world, face_count)
+
+offset = 40;
+
+x1 = world(1) + offset;
+y1 = world(2);
+x2 = world(3) + offset;
+y2 = world(4);
+
+start = [x1 y1];
+finish = [x2 y2];
+
+dd = face_count.face_data;
+
+for i = 1:length(dd(:,2))
+    x = dd(i,3);
+    y = dd(i,4);
+    [xy] = start_pix_to_world(x, y);
+    xy(1) = xy(1)  + offset;
+    obstacles(i,1) = xy(1);
+    obstacles(i,2) = xy(2);
+end
+
+start = floor(start/5);
+finish =  floor(finish/5);
+obstacles =  floor(obstacles/5);
+% obstacles = [obstacles; [4 2]];
+for i = 1:length(obstacles(:,1))
+    if (obstacles(i,1) == 0 || obstacles(i,2) == 0)
+        obstacles(i,:) = [];
+    end
+end
+path = get_path(start, finish, obstacles);
+end
